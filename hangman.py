fe86fc215d9319b709b953f3c8394a00f6cad063
Leonardo Nascimento <leocamello@gmail.com>
2016-02-03 22:57:27 -0200

    Improving helper code

--- hangman.py
+++ hangman.py
@@ -21,14 +21,14 @@ def loadWords():
     Depending on the size of the word list, this function may
     take a while to finish.
     """
-    print "Loading word list from file..."
+    print("Loading word list from file...")
     # inFile: file
     inFile = open(WORDLIST_FILENAME, 'r', 0)
     # line: string
     line = inFile.readline()
     # wordlist: list of strings
     wordlist = string.split(line)
-    print "  ", len(wordlist), "words loaded."
+    print("  " + str(len(wordlist)) + " words loaded.")
     return wordlist
 
 def chooseWord(wordlist):
